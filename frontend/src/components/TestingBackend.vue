12fef22a1168f43ea25d35ddf563c8700afa7fb1
Anthony Martin <anthonymartin20@gmail.com>
2023-05-21 15:21:14 -0400

    Frontend initial structure

---
+++ frontend/src/components/TestingBackend.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <button @click="login">Login</button>
+    <div>
+      {{ token }}
+    </div>
+    <button @click="getUsers">Get Users</button>
+    <ul>
+      <li v-for="user in users">
+        {{ user.email }}
+      </li>
+    </ul>
+    <button @click="logout">Logout</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: 'anthonymartin20@gmail.com',
+      password: '2601tony',
+      url: 'http://localhost:3000/',
+      token: '',
+      users: []
+    }
+  },
+
+  methods: {
+    login() {
+      const endpoint = 'auth/login';
+
+      this.axios.post(this.url + endpoint, {
+        email: this.email,
+        password: this.password,
+      })
+      .then((response) => {
+        console.log(response.data);
+        this.token = response.data.token;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    },
+    getUsers() {
+      const endpoint = 'users';
+
+      this.axios.get(this.url + endpoint, {
+        headers: {
+          Authorization: 'Bearer ' + this.token,
+        },
+      })
+      .then((response) => {
+        this.users = response.data;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    },
+    logout() {
+      const endpoint = 'auth/logout';
+
+      this.axios.post(this.url + endpoint, {
+        headers: {
+          Authorization: 'Bearer ' + this.token,
+        },
+      })
+      .then(() => {
+        this.token = '';
+        this.users = [];
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    }
+  },
+}
+</script>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script>
+  import TestingBackend from '@/components/TestingBackend.vue'
+
+  export default {
+    components: {
+      TestingBackend
+    }
+  }
+</script>
+
+<template>
+  <main>
+    <testing-backend />
+  </main>
+</template>
